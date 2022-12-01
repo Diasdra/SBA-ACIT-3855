43bd7ca84bda18dba418f0f1677f6b785432b87c
Diasdra <sayrial@gmail.com>
2022-11-30 18:30:26 -0800

    updated stuff:

--- Lab9/Audit/app.py
+++ Lab9/Audit/app.py
@@ -40,6 +40,7 @@ def get_return_car_application(index):
     logger.info("Retrieving return car at index %d" % index)
     try:
         pos = 0
+        logger.info(consumer)
         for msg in consumer:
             msg_str = msg.value.decode('utf-8')
             msg = json.loads(msg_str)
